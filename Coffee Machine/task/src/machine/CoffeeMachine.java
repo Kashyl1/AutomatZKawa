764d37434d7fc343731b7bca980fec9af00c24ab
Kashyl1 <92478936+Kashyl1@users.noreply.github.com>
2022-11-16 21:09:10 +0100

    Update CoffeeMachine.java

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-     RobienieKawy Kawa = new RobienieKawy();
+     RobienieKawy Kawa = new RobienieKawy(); // Tworzenie obiektu Kawy
      Kawa.obsluga();
     }
 }
